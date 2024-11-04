865a282311ba5951218f4e33ebbc75e7fd01511f
Marvin Heilemann <11534760+muuvmuuv@users.noreply.github.com>
2024-11-04 22:04:57 +0100

    feat: fix build errors and add more chunk modules

--- projects/app/src/main.ts
+++ projects/app/src/main.ts
@@ -1,12 +1,23 @@
-import { provideExperimentalZonelessChangeDetection } from "@angular/core"
+import {
+	provideAppInitializer,
+	provideExperimentalZonelessChangeDetection,
+} from "@angular/core"
 import { bootstrapApplication } from "@angular/platform-browser"
-import { provideRouter } from "@angular/router"
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async"
+import { provideRouter, withComponentInputBinding } from "@angular/router"
 
 import { AppComponent } from "./app/app.component"
 import { routes } from "./routes"
 
 bootstrapApplication(AppComponent, {
-	providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes)],
+	providers: [
+		provideExperimentalZonelessChangeDetection(),
+		provideRouter(routes, withComponentInputBinding()),
+		provideAnimationsAsync(),
+		provideAppInitializer(() => {
+			console.log("Initiated!")
+		}),
+	],
 }).catch((error) => {
 	console.error(error)
 })
