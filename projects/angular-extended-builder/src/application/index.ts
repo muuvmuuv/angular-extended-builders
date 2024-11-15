39b056cebc0cf9e657e14a9e6f30288b79beb590
Marvin Heilemann <11534760+muuvmuuv@users.noreply.github.com>
2024-11-15 16:41:44 +0100

    build: update to latest ng rc version

--- projects/angular-extended-builder/src/application/index.ts
+++ projects/angular-extended-builder/src/application/index.ts
@@ -1,23 +1,12 @@
 import * as path from "node:path"
 import { type BuilderContext, createBuilder } from "@angular-devkit/architect"
 import { getSystemPath, type json, normalize } from "@angular-devkit/core"
-import { buildApplication } from "@angular/build"
+import { type ApplicationBuilderExtensions, buildApplication } from "@angular/build"
 import type { IndexHtmlTransform } from "@angular/build/private"
-import type { Plugin } from "esbuild"
 import { defer, switchMap } from "rxjs"
 
 import { loadModule, loadPlugins } from "../load-plugins.js"
 import type { ExtendedApplicationBuilderOptions } from "../schema.js"
-
-// TODO: use Angular/build exported ApplicationBuilderExtensions once available
-/**
- * @see ./node_modules/@angular/build/src/builders/application/options.d.ts
- * @see https://github.com/angular/angular-cli/issues/28742#issuecomment-2445965941
- */
-interface ApplicationBuilderExtensions {
-	codePlugins?: Plugin[]
-	indexHtmlTransformer?: IndexHtmlTransform
-}
 
 function executeBuilder(options: ExtendedApplicationBuilderOptions, context: BuilderContext) {
 	const workspaceRoot = getSystemPath(normalize(context.workspaceRoot))
